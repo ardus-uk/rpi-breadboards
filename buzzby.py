e6fba7a33e6242152e0668e4361dc04ce64568d2
ardus-uk <peter@ardus.co.uk>
2019-12-03 08:51:35 +0000

    Correct typos

--- buzzby.py
+++ buzzby.py
@@ -3,12 +3,12 @@
 # Peter Normington
 # 2019-12-03
 
-from Buzzerboard import *
+from BuzzerBoard import *
 
 buzzer_setup = {
     "Whistle":20,
 }
 
-my_bb = Buzzerboard(buzzer_setup)
+my_bb = BuzzerBoard(buzzer_setup)
 my_bb.buzz(0.5)
 
