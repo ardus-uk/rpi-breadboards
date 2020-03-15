ac406958b40a93fba124499a1264e1e589882ab4
ardus-uk <peter@ardus.co.uk>
2020-03-15 18:11:59 +0000

    Use try-catch to end program

--- simplebutton.py
+++ simplebutton.py
@@ -7,13 +7,16 @@ GPIO.setmode(GPIO.BCM)
 ButtonPin=17
 GPIO.setup(ButtonPin, GPIO.IN)
 print(GPIO.input(ButtonPin))
-while True:
-    if GPIO.input(ButtonPin)==False:
-        print("Button Pressed")
-        print(GPIO.input(ButtonPin))
-        time.sleep(1)
-    else:
-        os.system('clear')
-        print("Waiting for a button press")
-        print(GPIO.input(ButtonPin))   
-    time.sleep(0.5)
+try:
+    while True:            
+        if GPIO.input(ButtonPin)==False:
+            print("Button Pressed")
+            print(GPIO.input(ButtonPin))
+            time.sleep(1)
+        else:
+            os.system('clear')
+            print("Waiting for a button press")
+            print(GPIO.input(ButtonPin))   
+        time.sleep(0.5)
+except (KeyboardInterrupt):
+    print("\nTest stopped")
