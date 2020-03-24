bce5a44b46a14f4c25ff1dfcada0eba16c136d43
ardus-uk <peter@ardus.co.uk>
2020-03-24 17:59:22 +0000

    Fix typo

--- 5buttons.py
+++ 5buttons.py
@@ -6,13 +6,13 @@
 
 from ButtonBoard import *
 
-5buttons_layout = [                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+5buttons_layout = {                                                                                                                                                                                                                                                                                                                                                                                                                                                            
             "Green":27,
             "Red":22,
             "White":23,
             "Blue":24,
             "Yellow":25,
-        ]
+        }
 
 5buttons_board = ButtonBoard(5buttons_layout)
 5buttons_board.test_all_buttons()
