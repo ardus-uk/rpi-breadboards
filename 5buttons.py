8c4bdee7f1e5d18483a7810f24d974cbc0b7e684
ardus-uk <peter@ardus.co.uk>
2020-03-24 17:47:50 +0000

    Add 5buttons.py - a breadboard with five buttons

---
+++ 5buttons.py
@@ -0,0 +1,12 @@
+import ButtonBoard
+
+5buttons_layout = {                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+            "Green":27,
+            "Red":22,
+            "White":23,
+            "Blue":24,
+            "Yellow":25,
+        }
+
+5buttons_board = ButtonBoard(5buttons_layout)
+5buttons_board.test_all_buttons()
