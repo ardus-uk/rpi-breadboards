72dfda2e9c772e2ddaa742e688ec1e3a28bb1494
ardus-uk <peter@ardus.co.uk>
2020-04-01 21:14:51 +0100

    Tidy code up

--- interrupt.py
+++ interrupt.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
+# With grateful acknowledgement to 
 # https://raspi.tv/2013/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3       
 import RPi.GPIO as GPIO  
 GPIO.setmode(GPIO.BCM)  
       
 # GPIO 23 & 17 set up as inputs, pulled up to avoid false detection.  
-# Both ports are wired to connect to GND on button press, 
-# so we'll be setting up falling edge detection for both. 
+# Each of these ports is wired to connect to GND on button press, 
+# so set up to detect falling edge detection on each.
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
   
 # GPIO 24 set up as an input, pulled down, connected to 3V3 on button press  
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
       
-# now we'll define two threaded callback functions  
-# these will run in another thread when our events are detected  
+# define two threaded callback functions  
+# which will run in another thread when events are detected  
 def my_callback(channel):  
     print ("\nfalling edge detected on 17") 
   
 def my_callback2(channel):  
     print ("\nfalling edge detected on 23")
       
+print ("INSTRUCTIONS\n")
+print ("------------\n")
 print ("Make sure you have a button connected so that when pressed")
 print ("it will connect GPIO port 23 (pin 16) to GND (pin 6)\n")
 print ("You will also need a second button connected so that when pressed")     
@@ -34,15 +37,12 @@ GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback, bouncetime=300)
   
 # when a falling edge is detected on port 23, regardless of whatever   
 # else is happening in the program, the function my_callback2 will be run  
-# 'bouncetime=300' includes the bounce control written into interrupts2a.py  
 GPIO.add_event_detect(23, GPIO.FALLING, callback=my_callback2, bouncetime=300)  
-
-      
+    
 try:  
     print ("\nWaiting for rising edge on port 24")  
     GPIO.wait_for_edge(24, GPIO.RISING)  
     print ("Rising edge detected on port 24. Here endeth the third lesson.")   
-      
 except KeyboardInterrupt:  
     GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
 GPIO.cleanup()           # clean up GPIO on normal exit  
