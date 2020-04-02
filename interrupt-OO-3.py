57dbfd95a2991347b17c5ef10825c1d72cbb0cfb
ardus-uk <peter@ardus.co.uk>
2020-04-02 18:13:23 +0100

    Add interrupt-oo-3.py, using gpiozero

---
+++ interrupt-OO-3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# interrupt-OO-3.py
+# Peter Normington
+# 2020-04-02
+
+# Object-oriented version of interrupt script
+
+from gpiozero import Buttons 
+
+def action1():
+    print("Hello, world!\n")
+  
+def action2():
+    print("Goodbye, universe!\n")
+
+
+btn = Button(17)
+btn.when_pressed = action1
+btn.when_released = action2
