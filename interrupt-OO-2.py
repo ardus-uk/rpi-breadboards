d7963519cb7bba8e0ccfd0bf4fef85fa896f64af
ardus-uk <peter@ardus.co.uk>
2020-04-02 09:38:52 +0100

    Add Down as class

--- interrupt-OO-2.py
+++ interrupt-OO-2.py
@@ -14,7 +14,7 @@ class UpInput:
 
 # Set up as inputs, pulled up to avoid false detection.  
 # Wired to connect to GND on button press, 
-# so detect falling edge detection on each.
+# so detect falling edge on each.
 
     def __init__(self, pin_button, action_function):
         self.pin_button = pin_button
@@ -30,13 +30,25 @@ class UpInput:
     def add_event_detector(self, bounce_time):
         GPIO.add_event_detect(self.pin_button, GPIO.FALLING, callback=self.call_back, bouncetime=bounce_time) 
 
+class DownInput:
+
+# Set up as inputs, pulled down initially.  
+# Wired to connect to 3V3 on button press, 
+# so detect rising edge.
+
+    def __init__(self, pin_button):
+        self.pin_button = pin_button
+        GPIO.setup(self.pin_button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    def edge_waiter(self):
+        GPIO.wait_for_edge(self.pin_button, GPIO.RISING) 
+
 # Test the class
 if __name__ == '__main__':
 
     def action1():
         print("Hello, world!\n")
-        
-    
+  
     def action2():
         print("Hello, universe!\n")
 
@@ -50,9 +62,7 @@ if __name__ == '__main__':
     print ("it will connect GPIO port 24 (pin 18) to 3V3 (pin 1)\n") 
     input("Press Enter when ready\n>")  
 
-
-    # GPIO 24 set up as an input, pulled down, connected to 3V3 on button press  
-    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
+    red = DownInput(24)  
     
     black = UpInput(17, action1)
     white = UpInput(23, action2)
@@ -61,7 +71,7 @@ if __name__ == '__main__':
     
     try:  
         print ("\nWaiting for rising edge on port 24")  
-        GPIO.wait_for_edge(24, GPIO.RISING)  
+        red.edge_waiter()  
         print ("\nRising edge detected on port 24\n\n--- END OF PROGRAM ---\n.")   
     except KeyboardInterrupt:
         # clean up GPIO on CTRL+C exit
