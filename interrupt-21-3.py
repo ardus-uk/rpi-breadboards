c7c9cc98d27dc5554b7202bcf07d9ada7859960c
ardus-uk <peter@ardus.co.uk>
2021-10-13 17:21:15 +0100

    Add exit routine

--- interrupt-21-3.py
+++ interrupt-21-3.py
@@ -6,7 +6,9 @@
 # Object-oriented version of interrupt script
 
 from gpiozero import Button 
-#import RPi.GPIO as GPIO  
+from subprocess import check_call
+from signal import pause
+import sys
 
 def actionw():
     print("Hello, white!\n")
@@ -14,15 +16,19 @@ def actionw():
 def actionb():
     print("Hello, black!\n")
 
+def stop_running():
+    sys.exit()
 
 white_btn = Button(16)
 black_btn = Button(20)
-red_btn = Button(21)
+exit_btn = Button(20, hold_time=3)
+#red_btn = Button(21)
 
 try:
     while True:
         white_btn.when_pressed = actionw
         black_btn.when_pressed = actionb
+        exit_btn.when_held = stop_running
         #white_btn.when_released = action2
 except KeyboardInterrupt:
     print("\nEnding program run\n")
