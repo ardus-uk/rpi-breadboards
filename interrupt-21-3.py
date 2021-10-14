7228fe7fd2148932a4815c42768159024ca03baf
ardus-uk <peter@ardus.co.uk>
2021-10-14 16:36:18 +0100

    Read from json config file

--- interrupt-21-3.py
+++ interrupt-21-3.py
@@ -9,9 +9,17 @@ from gpiozero import Button
 from subprocess import check_call
 from signal import pause
 import sys
+import json
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+jcf = os.path.join(script_dir,"config.json")
 
 def actionw():
-    print("Hello, white!\n")
+    with open(jcf) as json_cred_file:
+        cred = json.load(json_cred_file)
+    print("Hello,",cred['name'],"on White\n")
+    if (cred['name']=="Ruth"): print("EXTRA\n")
   
 def actionb():
     print("Hello, black!\n")
@@ -20,22 +28,16 @@ def stop_running():
     print("STOP (Red) pressed!\n")
     sys.exit()
 
-<<<<<<< HEAD
 white_btn = Button(16, bounce_time=0.05)
 black_btn = Button(20, bounce_time=0.05)
-red_btn = Button(21, hold_time = 2)
-=======
-white_btn = Button(16, bounce_time=0.5)
-#black_btn = Button(20)
-exit_btn = Button(20)
-#red_btn = Button(21)
->>>>>>> d085d09e87adbbe5c7cd42fd13f65c71d5288b9f
+red_btn = Button(21)
+
 
 try:
     while True:
         white_btn.when_pressed = actionw
         black_btn.when_pressed = actionb
-        red_btn.when_held = stop_running
+        red_btn.when_pressed = stop_running
         #white_btn.when_released = action2
 except KeyboardInterrupt:
     print("\nEnding program run\n")
