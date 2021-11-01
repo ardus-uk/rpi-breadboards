969bc0a77fb9faffb69bceeceb776d7782f381b4
ardus-uk <peter@ardus.co.uk>
2021-11-01 15:37:24 +0000

    Add exception to end prog run

--- mark1.py
+++ mark1.py
@@ -1,36 +1,44 @@
-# Using different button events to flash different LEDs
-# originally "flash_led_with_button.py"
-# MGB 1/11/21
+# Using button events to operate LEDs
+# originally "flash_led_with_button.py" from MGB 1/11/21
 #
 # C&C Buttons
 #
 
-#grn.blink(on_time=my_on_time, off_time=my_off_time, n=my_flashes,background=True)
-
-from gpiozero import LED
-from gpiozero import Button
+from gpiozero import LED, Button
 from signal import pause
+import sys
+import threading
 
 red = LED(16)
-yel = LED(20)
-grn = LED(21)
+grn = LED(20)
+yel = LED(21)
+
+btn_ops = Button(19)
+btn_stop = Button(26)
 
 def flash(btn,t_on,t_off,howmany):
     btn.blink(on_time=t_on, off_time=t_off, n=howmany,background=True)
 
-def say_hello():
-    print("Hello!")
+def pressed():
+    print("Pressed")
     flash(grn,1,0.5,3)
 
-def say_goodbye():
-    print("Goodbye!")
+def released():
+    print("Released")
     flash(red,0.5,1,3)
 
-button = Button(26)
+def stop_running():
+    print("STOP pressed!\n")
+    btn_ops.close()
+    sys.exit()
 
-button.when_pressed = say_hello
-button.when_released = say_goodbye
-#button.when_held = flash_green
-
-pause()    # This keep the main thread running
-button.close() 
+try:
+    while True:
+        btn_ops.when_pressed = pressed
+        btn_ops.when_released = released
+        btn_stop.when_pressed = stop_running()
+except KeyboardInterrupt:
+    print("Number of threads active: ",threading.active_count(),"\n")
+    print("Ending program run\n")
+    btn_ops.close()
+    sys.exit()
